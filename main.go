06f5af4e8850ee392fee25ec481f02e08c4a7668
marceloi <marcelo.ipar@gmail.com>
2025-07-09 12:44:41 -0300

    uso de funcion MuestroEnteros

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+//import "fmt"
+import "github.com/marcelo-a-ipar/GoDesdeCero/variables"
+
+func main() {
+	//	fmt.Println("¡Hola, mundo!")
+
+	variables.MuestroEnteros()
+
+}
---
+++ variables/enteras.go
@@ -0,0 +1,13 @@
+package variables
+
+import "fmt"
+
+func MuestroEnteros() {
+	var intcomun int
+	int32 := int32(10)
+	int64 := int64(100)
+
+	fmt.Println("intcomun:", intcomun)
+	fmt.Println("int32:", int32)
+	fmt.Println("int64:", int64)
+}
